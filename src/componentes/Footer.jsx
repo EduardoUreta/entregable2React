ba6fd2e71951adbc9be945a96bb1fceab1268280
Eduardo Ureta <eduardo.uretaca@gmail.com>
2023-07-22 13:36:43 -0400

    Arreglado footer

--- src/componentes/Footer.jsx
+++ src/componentes/Footer.jsx
@@ -2,7 +2,7 @@ export const Footer = () => {
     return (
         <div>
             <footer class="py45 bg-black footer">
-                <div class="container px-4 px-lg-5"><p class="m-0 text-center text-yellow-500">React JS x CH - Com. React JS 55245 - Eduardo Ureta</p></div>
+                <div class="container px-4 px-lg-5"><p class="m-0 text-center text-yellow-500">React JS x CH - Com. 55245 - Eduardo Ureta</p></div>
             </footer>
         </div>
     )
